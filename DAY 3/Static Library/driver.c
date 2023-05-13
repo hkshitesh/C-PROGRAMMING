d7d6abf507f2b3c285769aa77ececf53f6d7bd7a
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-05-13 15:22:10 +0530

    Add files via upload

---
+++ DAY 3/Static Library/driver.c
@@ -0,0 +1,7 @@
+#include "lib_mylib.h"
+#include<stdio.h>
+void main() 
+{
+  printf("\nSum=%d", sum(10,2));
+  printf("\nMul=%d", mul(10,2));
+}
---
+++ DAY 3/Static Library/lib_mylib.h
@@ -0,0 +1,4 @@
+int sum(int a, int b);
+int diff(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
