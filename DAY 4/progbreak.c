7d00993d983852300d78b0b8a7d303b58ebcbafb
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-05-20 15:53:56 +0530

    Add files via upload

---
+++ DAY 4/progbreak.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    int j=0;
+    for(i=0;i<5;i++)
+    {
+        printf("i= %d, j= ",i);
+        for(j=0;j<5;j++)
+        {
+            if(j==2)
+            {
+                break;
+            }
+            printf("%d ",j);
+        }
+        printf("\n");
+    }       
+    
+    return 0;
+}
